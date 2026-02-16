83f19cd4ec40fa2ed37859e9e54de81196ec5c0e
Smit Sakariya <smit.sakariya@shipmnts.com>
2026-02-16 18:07:58 +0530

    Override-Sequence-Number

--- app/models/sequence_generator/sequence.rb
+++ app/models/sequence_generator/sequence.rb
@@ -32,9 +32,12 @@ module SequenceGenerator
       sequence_prefix_without_digits, sequence_suffix = sequence_prefix.split(/#+/)
 
       # Use only sequence-driving prefix for lookup
-      next_number = CurrentSequence.get_next_number(sequence_prefix_without_digits, scope, purpose)
-
-      sequence_number = "%0#{digits}d" % (next_number).to_s
+      if options[:override_sequence_number].present?
+        sequence_number = options[:override_sequence_number]
+      else
+        next_number = CurrentSequence.get_next_number(sequence_prefix_without_digits, scope, purpose)
+        sequence_number = "%0#{digits}d" % (next_number).to_s
+      end
       prefix_without_digits + sequence_number + (suffix || '')
     end
 
